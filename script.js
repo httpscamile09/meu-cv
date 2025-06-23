507f3198b3caef0595bc3b7d45023cfc45700ab4
Camile Rodrigues Pereira <rcamile011@gmail.com>
2025-06-22 21:03:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function enviarEmail() {
+  window.location.href = "mailto:rcamile011@gmail.com";
+}
+
+function abrirGitHub() {
+  window.open("https://github.com/httpscamile09", "_blank");
+}
+
+function mostrarTelefone() {
+  alert("Número: (93) 99169-7028");
+}
+function abrirLinkedn() {
+    window.open("https://www.linkedin.com/feed/", "_blank");
+}
